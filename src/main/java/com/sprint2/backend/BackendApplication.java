8d8c691bc0c481ad6524f43d2428ccc250be4bd8
chauluong1995 <supea52795@gmail.com>
2021-01-03 13:12:45 +0700

    up

--- src/main/java/com/sprint2/backend/BackendApplication.java
+++ src/main/java/com/sprint2/backend/BackendApplication.java
@@ -6,10 +6,10 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
-public class BackEndApplication {
+public class BackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackEndApplication.class, args);
+        SpringApplication.run(BackendApplication.class, args);
     }
 
 }
