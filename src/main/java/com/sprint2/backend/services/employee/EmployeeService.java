c45422e7ddb68bb45e76b062cb66c15ab15e20ed
This PC <nguyentrandat9999@gmail.com>
2021-01-04 15:03:21 +0700

    dat_nt 04/01/2021 test push

--- src/main/java/com/sprint2/backend/services/employee/EmployeeService.java
+++ src/main/java/com/sprint2/backend/services/employee/EmployeeService.java
@@ -2,20 +2,37 @@ package com.sprint2.backend.services.employee;
 
 import java.util.List;
 
+import com.sprint2.backend.entity.AppAccount;
+import com.sprint2.backend.entity.AppRole;
 import com.sprint2.backend.entity.Employee;
 
 public interface EmployeeService {
-    List<Employee> findAll();
+    List<Employee> findAllEmployee();
 
-    Employee findByID(Long id);
+    Employee findEmployeeById(Long id);
 
-    void save(Employee employee);
+    void saveEmployee(Employee employee);
 
-    void update(Employee employee);
+    void editEmployee(Employee employee);
 
-    void deleteByID(Long id);
+    void deleteEmployee(Long id);
 
-    Employee findByFullName(String fullName);
 
-    Employee findByPosition(String position);
+    //------------------- Validate ton tai --------------------------
+    List<Employee> findEmployeeByCode(String code);
+
+    //------------------- Account - Role --------------------------
+    void saveAccount(AppAccount appAccount);
+
+    AppAccount findByNameAppAccount(String appAccount);
+
+    AppRole findByNameAppRole(String appRole);
+
+    List<AppRole> findAllByRole();
+    //-------------------------- Search -------------------------------
+    List<Employee> findEmployeeByFullNameContaining (String fullName);
+
+    List<Employee> findEmployeeByIdContaining (Long id);
+
+    List<Employee> findEmployeeByPositionContaining (String position);
 }
