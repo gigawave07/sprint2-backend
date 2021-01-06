40190153a8fb761d94e77aac2d2f197442e5a521
chauluong1995 <supea52795@gmail.com>
2021-01-06 08:21:35 +0700

    up 8h25 06/01

--- src/main/java/com/sprint2/backend/config/AutoSendMailForCustomer.java
+++ src/main/java/com/sprint2/backend/config/AutoSendMailForCustomer.java
@@ -165,7 +165,7 @@ public class AutoSendMailForCustomer {
                                 "Nếu không thanh toán vé sẽ được hủy\n" +
                                 "        theo quy định của công ty.</p>\n" +
                                 "      <p>Click vào <a style=\"color: green\" " +
-                                "href=\"http://localhost:4200\">đây</a> để đăng nhập nhanh vào hệ thống" +
+                                "href=\"http://localhost:4200\">đây</a> để đăng nhập nhanh vào hệ thống " +
                                 "của chúng tôi.</p>\n" +
                                 "      <p class=\"autoMail\">P/s : Đây là thư thông báo tự động. " +
                                 "Quý khách vui lòng không trả lời thư này!</p>\n" +
