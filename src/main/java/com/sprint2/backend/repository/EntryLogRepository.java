b0e97dc72ab59b6ded07096034ebe7a27aae5c07
nhatprovklno3 <nguyenhongnhat71298@gmail.com>
2021-01-07 10:43:37 +0700

    update 7/1/2021 backend complete update save entrylog

--- src/main/java/com/sprint2/backend/repository/EntryLogRepository.java
+++ src/main/java/com/sprint2/backend/repository/EntryLogRepository.java
@@ -1,10 +1,16 @@
 package com.sprint2.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.sprint2.backend.entity.EntryLog;
 
+import java.util.List;
+
 @Repository
 public interface EntryLogRepository extends JpaRepository<EntryLog, Long> {
+    @Query(nativeQuery = true, value = "SELECT * FROM entry_log\n" +
+            "where member_card_id = ?;")
+    List<EntryLog> getEntryLogByMemberCard(Long idMember);
 }
