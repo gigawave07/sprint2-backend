4bbd0fb33cefa3f462a30ee9019e4d4d09f280dd
nhatlanh2202 <nhatlanh2202@gmail.com>
2021-01-10 21:35:30 +0700

    lanh befor merge update complete 00h10 11/01

--- src/main/java/com/sprint2/backend/repository/MemberCardRepository.java
+++ src/main/java/com/sprint2/backend/repository/MemberCardRepository.java
@@ -5,14 +5,10 @@ import com.sprint2.backend.entity.MemberCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 
 @Repository
 public interface
 MemberCardRepository extends JpaRepository<MemberCard, Long> {
-    /**
-     * Lanh
-     * @param plateNumber
-     * @return MemberCard
-     */
-    MemberCard findByCar_PlateNumber(String plateNumber);
 }
