6215a3b3a6f4507be4d54609e6ccd5310a814b52
Hoat <vanhoat1631997@gmail.com>
2021-01-11 09:33:19 +0700

    Hoat update complete

--- src/main/java/com/sprint2/backend/controller/CustomerController.java
+++
@@ -1,9 +0,0 @@
-package com.sprint2.backend.controller;
-
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController("/customer")
-@CrossOrigin
-public class CustomerController {
-}
