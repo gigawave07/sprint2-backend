d2143eefee8778598d49fda3fcc9315af95e5679
Gigawave07 <gigawave07@gmail.com>
2021-01-11 09:32:00 +0700

    danh fix view

--- src/main/java/com/sprint2/backend/repository/TicketRepository.java
+++ src/main/java/com/sprint2/backend/repository/TicketRepository.java
@@ -18,7 +18,7 @@ public interface TicketRepository extends JpaRepository<Ticket, Long> {
      * @return Thống kê doanh thu trong khoảng thời gian (ticket)
      */
     @Query(nativeQuery = true, value = "select json_arrayagg(json_object" +
-            "('enter_date', project2_parking_management.statistic_total_revenue_ticket.date_payment,\n" +
+            "('exit_date', project2_parking_management.statistic_total_revenue_ticket.date_payment,\n" +
             "'total_price', project2_parking_management.statistic_total_revenue_ticket.total_price))\n" +
             "from project2_parking_management.statistic_total_revenue_ticket\n" +
             "where project2_parking_management.statistic_total_revenue_ticket.date_payment between (?1) and (?2);")
