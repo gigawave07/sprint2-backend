977db09497039870084f5e7b79b7bd4438c1fe4b
chauluong1995 <supea52795@gmail.com>
2021-01-11 08:22:21 +0700

    Chau merge Lanh complete 8h25 11/01

--- src/main/java/com/sprint2/backend/controller/MemberCardController.java
+++ src/main/java/com/sprint2/backend/controller/MemberCardController.java
@@ -12,21 +12,21 @@ import com.sprint2.backend.model.MemberCardListDTO;
 import com.sprint2.backend.entity.Car;
 import com.sprint2.backend.entity.MemberCardType;
 import com.sprint2.backend.entity.ParkingSlot;
-import com.sprint2.backend.services.lanh_nqn.LanhService;
+import com.sprint2.backend.services.member_card.MemberCardService;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/lanh/member-card")
 public class MemberCardController {
     @Autowired
-    private LanhService lanhService;
+    private MemberCardService memberCardService;
 
     /**
      * Lành start
      */
     @PostMapping("/add")
     public ResponseEntity<?> addMemberCard(@RequestBody MemberCardAddDTO memberCardAddDTO) {
-        this.lanhService.save(memberCardAddDTO);
+        this.memberCardService.saveDTO(memberCardAddDTO);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
@@ -34,31 +34,31 @@ public class MemberCardController {
 
     @GetMapping("/list")
     public ResponseEntity<List<MemberCardListDTO>> getMemberCardListDemo() {
-        List<MemberCardListDTO> memberCardListDTOList = this.lanhService.findAllMemberCardDTO();
+        List<MemberCardListDTO> memberCardListDTOList = this.memberCardService.findAllMemberCardDTO();
         return ResponseEntity.ok(memberCardListDTOList);
     }
 
     @GetMapping("/find-plate-number/{plateNumber}")
     public ResponseEntity<List<Car>> getEmployeeByCode(@PathVariable String plateNumber) {
-        List<Car> carList = this.lanhService.findPlateNumber(plateNumber);
+        List<Car> carList = this.memberCardService.findPlateNumber(plateNumber);
         return new ResponseEntity<>(carList, HttpStatus.OK);
     }
 
     @GetMapping("/parkingSlot")
     public ResponseEntity<List<ParkingSlot>> getParkingSlot() {
-        List<ParkingSlot> parkingSlotList = this.lanhService.findAllParkingSlotNeed();
+        List<ParkingSlot> parkingSlotList = this.memberCardService.findAllParkingSlotNeed();
         return ResponseEntity.ok(parkingSlotList);
     }
 
     @GetMapping("/memberCardType")
     public ResponseEntity<List<MemberCardType>> getAll() {
-        List<MemberCardType> memberCardTypeList = this.lanhService.findAllMemberCardType();
+        List<MemberCardType> memberCardTypeList = this.memberCardService.findAllMemberCardType();
         return ResponseEntity.ok(memberCardTypeList);
     }
 
     @GetMapping("/search-plate-number/{plateNumber}")
     public ResponseEntity<List<MemberCardListDTO>> searchPlateNumber(@PathVariable String plateNumber) {
-        List<MemberCardListDTO> memberCardListDTOS = this.lanhService.findByCarPlateNumber(plateNumber);
+        List<MemberCardListDTO> memberCardListDTOS = this.memberCardService.findByCarPlateNumber(plateNumber);
         return new ResponseEntity<>(memberCardListDTOS, HttpStatus.OK);
     }
 
--- src/main/java/com/sprint2/backend/services/member_card/MemberCardServiceImpl.java
+++ src/main/java/com/sprint2/backend/services/member_card/MemberCardServiceImpl.java
@@ -1,12 +1,15 @@
 package com.sprint2.backend.services.member_card;
 
-import com.sprint2.backend.entity.MemberCard;
+import com.sprint2.backend.entity.*;
+import com.sprint2.backend.model.MemberCardAddDTO;
+import com.sprint2.backend.model.MemberCardListDTO;
 import com.sprint2.backend.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -14,6 +17,17 @@ public class MemberCardServiceImpl implements MemberCardService {
     @Autowired
     private MemberCardRepository memberCardRepository;
 
+    @Autowired
+    private MemberCardTypeRepository memberCardTypeRepository;
+
+    @Autowired
+    private CarRepository carRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private ParkingSlotRepository parkingSlotRepository;
 
     @Override
     public List<MemberCard> findAll() {
@@ -135,4 +149,109 @@ public class MemberCardServiceImpl implements MemberCardService {
     }
 
     // ----------------------------- Vinh End ------------------------------------------
+
+    /**
+     * Lành
+     */
+    @Override
+    public List<MemberCardListDTO> findAllMemberCardDTO() {
+        List<MemberCardListDTO> memberCardListDTOList = new ArrayList<>();
+
+        List<MemberCard> memberCardList = this.memberCardRepository.findAll();
+        for (MemberCard memberCard : memberCardList) {
+            MemberCardListDTO memberCardListDTO = new MemberCardListDTO();
+            memberCardListDTO.setId(memberCard.getId());
+            memberCardListDTO.setStartDate(String.valueOf(memberCard.getStartDate()));
+            memberCardListDTO.setEndDate(String.valueOf(memberCard.getEndDate()));
+            memberCardListDTO.setPrice(memberCard.getPrice());
+            memberCardListDTO.setNameType(memberCard.getMemberCardType().getMemberTypeName());
+            memberCardListDTO.setPlateNumber(memberCard.getCar().getPlateNumber());
+            memberCardListDTO.setNameCustomer(memberCard.getCar().getCustomer().getFullName());
+            memberCardListDTO.setFloor(memberCard.getCar().getParkingSlot().getFloor());
+            memberCardListDTO.setSlotNumber(memberCard.getCar().getParkingSlot().getSlotNumber());
+            memberCardListDTOList.add(memberCardListDTO);
+        }
+        return memberCardListDTOList;
+    }
+
+
+    @Override
+    public void saveDTO(MemberCardAddDTO memberCardAddDTO) {
+        MemberCard memberCard = new MemberCard();
+        Car car = new Car();
+        Customer customer = new Customer();
+
+        // create Customer :
+        customer.setFullName(memberCardAddDTO.getFullName());
+        this.customerRepository.save(customer);
+
+        // create Car :
+        car.setPlateNumber(memberCardAddDTO.getPlateNumber());
+        car.setCustomer(this.customerRepository.findByFullName(memberCardAddDTO.getFullName()));
+        this.carRepository.save(car);
+
+        // set SlotNumber :
+        ParkingSlot parkingSlot;
+        parkingSlot = this.parkingSlotRepository.findById(memberCardAddDTO.getSlotNumber()).orElse(null);
+        if (parkingSlot != null) {
+            parkingSlot.setStatus(true);
+            parkingSlot.setReserved(true);
+            parkingSlot.setCar(car);
+            this.parkingSlotRepository.save(parkingSlot);
+        }
+
+        // set properties of memberCard :
+        memberCard.setMemberCardType(
+                this.memberCardTypeRepository.findById(memberCardAddDTO.getMemberTypeID()).orElse(null));
+        memberCard.setStartDate(memberCardAddDTO.getStartDate());
+        memberCard.setEndDate(memberCardAddDTO.getEndDate());
+        memberCard.setPrice(memberCardAddDTO.getPrice());
+        memberCard.setCar(this.carRepository.findByPlateNumber(memberCardAddDTO.getPlateNumber()));
+        this.memberCardRepository.save(memberCard);
+    }
+
+    @Override
+    public List<MemberCardListDTO> findByCarPlateNumber(String plateNumber) {
+        List<MemberCardListDTO> memberCardListDTOList = new ArrayList<>();
+        List<MemberCard> memberCardList = this.memberCardRepository.findAll();
+        for (MemberCard memberCard : memberCardList) {
+            if (memberCard.getCar().getPlateNumber().equals(plateNumber)) {
+                MemberCardListDTO memberCardListDTO = new MemberCardListDTO();
+                memberCardListDTO.setId(memberCard.getId());
+                memberCardListDTO.setStartDate(String.valueOf(memberCard.getStartDate()));
+                memberCardListDTO.setEndDate(String.valueOf(memberCard.getEndDate()));
+                memberCardListDTO.setPrice(memberCard.getPrice());
+                memberCardListDTO.setNameType(memberCard.getMemberCardType().getMemberTypeName());
+                memberCardListDTO.setPlateNumber(memberCard.getCar().getPlateNumber());
+                memberCardListDTO.setNameCustomer(memberCard.getCar().getCustomer().getFullName());
+                memberCardListDTO.setFloor(memberCard.getCar().getParkingSlot().getFloor());
+                memberCardListDTO.setSlotNumber(memberCard.getCar().getParkingSlot().getSlotNumber());
+                memberCardListDTOList.add(memberCardListDTO);
+            }
+        }
+        return memberCardListDTOList;
+    }
+
+
+    @Override
+    public List<Car> findPlateNumber(String plateNumber) {
+        return this.carRepository.findCarByPlateNumber(plateNumber);
+    }
+
+    @Override
+    public List<MemberCardType> findAllMemberCardType() {
+        return this.memberCardTypeRepository.findAll();
+    }
+
+    @Override
+    public List<ParkingSlot> findAllParkingSlotNeed() {
+        List<ParkingSlot> parkingSlotListExists = this.parkingSlotRepository.findAll();
+        List<ParkingSlot> parkingSlotListDisplay = new ArrayList<>();
+        for (ParkingSlot parkingSlot : parkingSlotListExists) {
+            if (!parkingSlot.getReserved() && !parkingSlot.getStatus()) {
+                parkingSlotListDisplay.add(parkingSlot);
+            }
+        }
+        return parkingSlotListDisplay;
+    }
 }
